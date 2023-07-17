7ccb18be2dae3c82e7ebab80f927d0792ea8321f
Andreas <andreas.landerer@vontobel.com>
2023-07-17 13:54:52 +0200

    Initial commit

---
+++ src/main/kotlin/com/landerer/argocd/tutorial/TutorialController.kt
@@ -0,0 +1,13 @@
+package com.landerer.argocd.tutorial
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/tutorial")
+class TutorialController {
+
+    @GetMapping
+    fun greeting() = "Hello, World!"
+}
